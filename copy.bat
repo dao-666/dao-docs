2fc4534692f970f41f575b9cd8971567d54a94b1
starvv_w@qq.com <starvv_w@qq.com>
2024-02-21 14:06:02 +0800

    单独出来

--- copy.bat
+++
@@ -1,9 +0,0 @@
-rem rd unpackage /s /q 
-
-cd .\docs\.vitepress
-rem xcopy dist\. ..\..\unpackage\dist\build\h5\. /y/e/i
-
-rd C:\dDisk\0.uniapp.demo\dao-docs\unpackage /s/q 
-xcopy dist\. C:\dDisk\0.uniapp.demo\dao-docs\unpackage\dist\build\h5\. /y/e/i
-
-pause
